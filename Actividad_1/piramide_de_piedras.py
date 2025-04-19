d586cc6a4fbb92096228fa3b1a795931def002f9
Aneprados <apradoscamara@gmail.com>
2025-04-19 11:26:17 +0100

    creamos la clase mover piedras

--- Actividad_1/piramide_de_piedras.py
+++ Actividad_1/piramide_de_piedras.py
@@ -1,30 +1,4 @@
 class PiramideDePiedras:
-    def __init__(self, numero_de_piedras):
-        """
-        Inicializa la pirámide con el número de piedras.
-        
-        :param numero_de_piedras: Número total de piedras en la pirámide.
-        """
-        self.numero_de_piedras = numero_de_piedras
-
-    def mover_piedras(self, n, origen, destino, auxiliar):
-        """
-        Resuelve el problema de mover n piedras de una columna a otra siguiendo las reglas.
-        
-        :param n: Número de piedras a mover.
-        :param origen: Nombre de la columna origen.
-        :param destino: Nombre de la columna destino.
-        :param auxiliar: Nombre de la columna auxiliar.
-        """
-        if n == 1:
-            print(f"Mueve la piedra de {origen} a {destino}")
-            return
-        # Mueve n-1 piedras de la columna origen a la columna auxiliar
-        self.mover_piedras(n - 1, origen, auxiliar, destino)
-        # Mueve la piedra más grande de la columna origen a la columna destino
-        print(f"Mueve la piedra de {origen} a {destino}")
-        # Mueve las n-1 piedras de la columna auxiliar a la columna destino
-        self.mover_piedras(n - 1, auxiliar, destino, origen)
 
     def resolver(self):
         """
