4f7366018007760b6e0c675a04106ff15709cdf5
Aneprados <apradoscamara@gmail.com>
2025-04-22 15:58:40 +0100

    la clase lanzador

--- Actividad_4/main.py
+++ Actividad_4/main.py
@@ -1,24 +1,4 @@
+from Lanzador import lanzador
 if __name__ == "__main__":
-    p1 = Polinomio()
-    p1.agregar_termino(3, 2)
-    p1.agregar_termino(5, 1)
-
-    p2 = Polinomio()
-    p2.agregar_termino(1, 1)
-    p2.agregar_termino(2, 0)
-
-    print("Polinomio 1:", p1)
-    print("Polinomio 2:", p2)
-
-    resta = p1.restar(p2)
-    print("Resta:", resta)
-
-    cociente, residuo = p1.dividir(p2)
-    print("Cociente:", cociente)
-    print("Residuo:", residuo)
-
-    p1.eliminar_termino(1)
-    print("Polinomio 1 después de eliminar término con exponente 1:", p1)
-
-    existe = p1.existe_termino(2)
-    print("¿Existe término con exponente 2 en Polinomio 1?", existe)
+    lanzador = lanzador()
+    lanzador.ejecutar()
